a2ee880d4c9aa63626c00d85fd8020c94d0acb5c
Maxim Shirshin <maxim.shirshin@shopify.com>
2022-05-20 17:21:05 +0200

    Restore commands back

--- worker-dist/index.js
+++
@@ -1,6 +0,0 @@
-addEventListener("fetch", (e) => {
-        const resp = new Response("Test response that works for sure", {
-                status: 200
-        });
-        e.respondWith(resp);
-})
