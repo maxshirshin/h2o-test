ba1774c1ef5e9ba56f8a3074c57c6577b222376b
Maxim Shirshin <maxim.shirshin@shopify.com>
2022-05-31 11:20:15 +0200

    Using "client" as the assets dir since DMS has the path hardcoded

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,7 @@
  */
  module.exports = {
   appDirectory: "app",
-  browserBuildDirectory: "public/build",
+  browserBuildDirectory: "public/client",
   publicPath: process.env.NODE_ENV === "production" ? process.env.HYDROGEN_ASSET_BASE_URL : "/build/",
   serverBuildDirectory: "build",
   devServerBroadcastDelay: 1000
